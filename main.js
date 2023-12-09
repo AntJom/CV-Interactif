f390a1fb499e4aedf2e6b5760e95bf02c9d24b08
AntJom <102387616+AntJom@users.noreply.github.com>
2023-12-09 11:28:43 +0100

    Add files via upload
    Version Antonin avant passage Romain

--- main.js
+++ main.js
@@ -16,24 +16,35 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // La deuxième vale
 scene.add(ambientLight);
 
 let ground;
-let model;
-let bat1;
+let car;
+let icam;
+let maison;
 let cottage;
 let groundparking1;
+let groundparking2;
 
 // Créer une boîte englobante pour la voiture
 const carBoundingBox = new THREE.Box3();
 
 // Créer une boîte englobante pour l'objet
-const bat1BoundingBox = new THREE.Box3();
-
+const icamBoundingBox = new THREE.Box3();
 const cottageBoundingBox = new THREE.Box3();
 const parking1BoundingBox = new THREE.Box3();
+const maisonBoundingBox = new THREE.Box3();
+const parking2BoundingBox = new THREE.Box3();
 
 const geometry = new THREE.BoxGeometry(10,3,5);
 const material = new THREE.MeshBasicMaterial({  color: 0x00ff00 , transparent: true , opacity : 0});
 const parking1 = new THREE.Mesh( geometry, material );
+parking1.position.y = -0.28;
+parking1.position.z = -20;
 scene.add(parking1);
+const parking2 = new THREE.Mesh( geometry, material );
+parking2.position.x = -22;
+parking2.position.z = 0;
+parking2.position.y = 0;
+scene.add(parking2);
+
 
 
 const direction = new THREE.Vector3(1, 0, 0); // Vecteur de direction initial (vers la droite)
@@ -48,30 +59,32 @@ const coordinatesDiv = document.getElementById('coordinates');
 
 const loader = new GLTFLoader();
 loader.load('models/voiture.glb', (gltf) => {
-  model = gltf.scene;
-  scene.add(model);
+  car = gltf.scene;
+  scene.add(car);
   // Réinitialiser la rotation à zéro
-  model.rotation.set(0, 0, 0);
-  model.scale.set(2,2,2)
+  car.rotation.set(0, 0, 0);
+  car.scale.set(2,2,2)
 
   // Tourner le modèle de 180 degrés autour de l'axe Y
 
 
 
-  const box = new THREE.Box3().setFromObject(model);
+  const box = new THREE.Box3().setFromObject(car);
     const center = box.getCenter(new THREE.Vector3());
 
     controls.target.copy(center);
     controls.update();
-    carBoundingBox.setFromObject(model);
+    carBoundingBox.setFromObject(car);
 });
 
-loader.load( 'models/immeuble.glb', (gltf) => { //initie le téléchargement du modèle 3D
-  bat1 = gltf.scene;
-  bat1.position.x = 30;
-  bat1.position.y = 7;
-  bat1.scale.set(75,75,75)
-  scene.add(bat1);
+loader.load( 'models/icam.glb', (gltf) => { //initie le téléchargement du modèle 3D
+  icam = gltf.scene;
+  icam.position.x = 3;
+  icam.position.y = 0.5;
+  icam.position.z = -50;
+  icam.scale.set(5,5,5);
+  icam.rotation.set(0, Math.PI, 0);
+  scene.add(icam);
   renderer.render( scene, camera );
 
 });
@@ -84,6 +97,20 @@ cottage.scale.set(10,10,10)
  scene.add(cottage);
  renderer.render( scene, camera );
 });
+
+loader.load( 'models/maison_colombage.glb',  (gltf) => {
+
+  maison = gltf.scene;
+  maison.position.x = -50;
+  maison.position.y = -0.5;
+  maison.position.z = 0;
+  maison.scale.set(6, 6, 6);
+  maison.rotation.set(0, Math.PI/2, 0);
+  scene.add(maison);
+  renderer.render( scene, camera );
+});
+
+
 
 
 
@@ -101,14 +128,30 @@ ground.rotation.x = -Math.PI / 2; // Rotation pour placer le sol à l'horizontal
 ground.position.y = -0.29;
 scene.add(ground);
 
-const parking1Texture = textureLoader.load('models/parking.jpg');
-const parking1Geometry = new THREE.PlaneGeometry(10, 10);
-const parking1Material = new THREE.MeshBasicMaterial({ map: parking1Texture, side: THREE.DoubleSide });
-groundparking1 = new THREE.Mesh(parking1Geometry, parking1Material);
+
+
+
+
+
+const parkingTexture = textureLoader.load('models/parking.jpg');
+const parkingGeometry = new THREE.PlaneGeometry(10, 10);
+const parkingMaterial = new THREE.MeshBasicMaterial({ map: parkingTexture, side: THREE.DoubleSide });
+groundparking1 = new THREE.Mesh(parkingGeometry, parkingMaterial);
 groundparking1.rotation.x = -Math.PI / 2; // Rotation pour placer le sol à l'horizontale
 groundparking1.position.y = -0.28;
+groundparking1.position.z = -20;
 scene.add(groundparking1);
-// Ajouter le cube au monde
+
+groundparking2 = new THREE.Mesh(parkingGeometry, parkingMaterial);
+groundparking2.rotation.x = -Math.PI / 2; // Rotation pour placer le sol à l'horizontale
+groundparking2.position.x = -20;
+groundparking2.position.y =-0;
+groundparking2.position.z = 0;
+scene.add(groundparking2);
+
+
+
+
 
 
 // Configuration déplacements
@@ -158,11 +201,8 @@ function handleKeyEvent(keyLetter, isPressed) {
       case 'ArrowRight':
           keys.right = isPressed;
           break;
-      case 'a':
-          keys.a = isPressed;
-          break;
-      case 'q':
-         keys.q = isPressed;
+      case 'Enter':
+         keys.enter = isPressed;
           break;
   }
 };
@@ -172,10 +212,11 @@ function handleKeyEvent(keyLetter, isPressed) {
 function animate() {
   requestAnimationFrame(animate); //rapelle la fonction animate à chaque rafraîchissement d'écran
   renderer.render(scene, camera); // ordonne au renderer de rendre la scene ainsi que le PDV de la caméra 
+
   
 
 
-  if (model) {
+  if (car) {
 
     speed=0;
     
@@ -217,25 +258,18 @@ function animate() {
       speed = 10;
     }
 
-    // if (keys.a) {
-    //    speed += 0.1;
-    // }
-
-    // if (keys.q) {
-    //     speed -= 0.1;
-    // }
   };
   moveCar();
-  controls.target.copy(model.position);
+  controls.target.copy(car.position);
   controls.update();   // Mettez à jour les contrôles lors de l'animation
-  model.lookAt(model.position.clone().add(direction));
-  model.rotation.y += Math.PI;
-  camera.position.y = model.position.y + 10;
-  camera.position.x = model.position.x ;
-  camera.position.z = model.position.z + 15 ;
-  if (model) {
+  car.lookAt(car.position.clone().add(direction));
+  car.rotation.y += Math.PI;
+  camera.position.y = car.position.y + 10;
+  camera.position.x = car.position.x ;
+  camera.position.z = car.position.z + 15 ;
+  if (car) {
     // Mettre à jour la boîte englobante de la voiture à chaque frame
-    carBoundingBox.setFromObject(model);
+    carBoundingBox.setFromObject(car);
 
     // ... (autres mises à jour d'animation) osef c'est gpt
 
@@ -243,28 +277,39 @@ function animate() {
     checkCollision();
   }
 
-  const carPosition = camera.position;
+
+  const carPosition = car.position;
   coordinatesDiv.textContent = `Position de la voiture : x: ${carPosition.x.toFixed(1)}, y: ${carPosition.y.toFixed(1)}, z: ${carPosition.z.toFixed(1)}`;
 
 
 };
 
+
+
+
+
 const moveCar = () => {
   const delta = clock.getDelta(); // Delta de temps entre les frames
   const displacement = new THREE.Vector3();
   displacement.copy(direction).multiplyScalar(speed * delta);
-  model.position.add(displacement);
+  car.position.add(displacement);
+
 
 };
 
+
+
+
+
 function checkCollision() {
   // Mettre à jour la boîte englobante de l'objet
-  bat1BoundingBox.setFromObject(bat1);
+  icamBoundingBox.setFromObject(icam);
   cottageBoundingBox.setFromObject(cottage);
   parking1BoundingBox.setFromObject(parking1);
+  parking2BoundingBox.setFromObject(parking2);
 
   // Vérifier la collision
-  if (carBoundingBox.intersectsBox(bat1BoundingBox)) {
+  if (carBoundingBox.intersectsBox(icamBoundingBox)) {
     // Collision détectée, prenez des mesures ici (par exemple, arrêtez la voiture)
     console.log('Collision détectée!');
     camera.position.x=50;
@@ -277,22 +322,67 @@ function checkCollision() {
     camera.position.y=12;
   }
 
+  
+
   if (carBoundingBox.intersectsBox(parking1BoundingBox)) {
     // Collision détectée, prenez des mesures ici (par exemple, arrêtez la voiture)
     console.log('Collision détectée!');
-    camera.position.z=-27.9;
-    camera.position.y=8.5;
-    camera.position.x=-38.4;
-    parking1Geometry.geometry.dispose();
-    parking1Geometry = new THREE.PlaneGeometry(50,50);
-    const parking1Material = new THREE.MeshBasicMaterial({ map: parking1Texture, side: THREE.DoubleSide });
-    groundparking1 = new THREE.Mesh(parking1Geometry, parking1Material);
-    scene.add(groundparking1);
-  }
-  
+    camera.position.z = 15;
+    camera.position.y = 14;
+    camera.position.x = -21;
 
+    
+if (keys.enter){
+  //window.open('page.html', 'Spam', 'width=500,height=300,left=100,top=100,toolbar=no,scrollbars=yes');  POUR OUVRIR LA PAGE WEB
+  console.log('Page chargée');
 }
 
+  }
+
+  if (carBoundingBox.intersectsBox(parking2BoundingBox)) {
+    // Collision détectée, prenez des mesures ici (par exemple, arrêtez la voiture)
+    console.log('Collision détectée!');
+    camera.position.z = 0;
+    camera.position.y = 15;
+    camera.position.x = 0;
+
+    
+if (keys.enter){
+  //window.open('page.html', 'Spam', 'width=500,height=300,left=100,top=100,toolbar=no,scrollbars=yes');  POUR OUVRIR LA PAGE WEB
+  console.log('Page chargée');
+}
+  }
+}
 animate();
 
 
+
+
+
+
+
+
+// Finir système place de parking
+// utiliser la touche "enter" sur une place de parking pour ouvrir une place de parking --> pr que ca soit que quand on leve la touche
+// placer évents + objet map
+// tuto utilisation du portfolio 3D
+
+//+ Regarder pour faire des lettres en 3D
+
+
+
+//RAPPORT
+//expliquer outil vite (NOTE PART OUTILS ET LOG.) xxxxxxx
+//placer modeles map xxxxxxxx
+//ajouter pgn github XXXXXXX
+//finir 2/3 trucs moche code XXXXXX
+//readme (TUTO POUR PROF INSTALLATION) xxxxxx
+//video pr présenter proj A FAIRE
+
+
+    //Code pour faire grandir et réctrécir la place de parking (JE FAIS CA SEMAINE PRO SHORT POUR DIMANCHE)
+    //parkingGeometry.geometry.dispose();
+    //parkingGeometry = new THREE.PlaneGeometry(50,50);
+    //const parkingMaterial = new THREE.MeshBasicMaterial({ map: parkingTexture, side: THREE.DoubleSide });
+    //groundparking1 = new THREE.Mesh(parkingGeometry, parkingMaterial);
+    //scene.add(groundparking1);
